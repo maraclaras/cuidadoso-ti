8efbc2f23591d7adcf96531d62861ad6c428e76d
Maria Clara <maria.silva.1328962@sga.pucminas.br>
2024-04-23 18:56:17 -0300

    first commit

---
+++ cuidadoso/src/main/java/com/ti2/cuidadoso/CuidadosoApplication.java
@@ -0,0 +1,13 @@
+package com.ti2.cuidadoso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuidadosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuidadosoApplication.class, args);
+	}
+
+}
---
+++ cuidadoso/src/test/java/com/ti2/cuidadoso/CuidadosoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ti2.cuidadoso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuidadosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
